73fcc671b1b1883bbc160e0cce0970722d89d9f5
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2022-04-16 19:40:25 +0300

    Added Reactstrap

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import { Button } from 'reactstrap';
 
 function App() {
   return (
     <div className="App">
-      
+      <Button className='bg-primary'>My first button</Button>
     </div>
   );
 }
