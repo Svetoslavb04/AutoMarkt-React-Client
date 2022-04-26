d9691fca4e0d005260bebd66cf6436c19417aedb
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2022-04-26 21:29:16 +0300

    Added Service Cards in Home

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,7 +2,8 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import { Autoplay } from "swiper";
 import {
-    StyledEngineProvider, Box, Button
+    StyledEngineProvider, Box, Button,
+    Grid
 } from '@mui/material';
 import image1 from './carousel-1-min.jpg';
 import image2 from './carousel-2-min.jpg';
@@ -29,7 +30,20 @@ export default function Home() {
                     <SwiperSlide><img src={image3} alt="Image 3" /></SwiperSlide>
                 </Swiper>
             </Box>
-
+            <Grid container className='home-services-grid'>
+                <Grid item xs={12} sm={4} md={2} className='home-services-grid-item'>
+                    <Button className='home-services-grid-item-content' variant="outlined" disableRipple>Sell for Free</Button>
+                </Grid>
+                <Grid item xs={12} sm={4} md={2} className='home-services-grid-item'>
+                    <Button className='home-services-grid-item-content' variant="outlined" disableRipple>Cash on delivery</Button>
+                </Grid>
+                <Grid item xs={12} sm={4} md={2} className='home-services-grid-item'>
+                    <Button className='home-services-grid-item-content' variant="outlined" disableRipple>Vehicle history</Button>
+                </Grid>
+                <Grid item xs={12} sm={4} md={2} className='home-services-grid-item'>
+                    <Button className='home-services-grid-item-content' variant="outlined" disableRipple>Clear records</Button>
+                </Grid>
+            </Grid>
         </StyledEngineProvider>
     );
 }
