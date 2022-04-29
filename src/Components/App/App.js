f0c53ac2eeb59847d49cf62dbb928f55dd3ec95d
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2022-04-29 10:43:56 +0300

    Add Auth Context

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -7,21 +7,31 @@ import { Routes, Route } from 'react-router-dom';
 import Home from '../../Pages/Home/Home';
 import Login from '../../Pages/Login/Login';
 import Register from '../../Pages/Register/Register';
-
+import { AuthContext } from '../../contexts/AuthContext.js';
+import * as React from 'react';
 
 function App() {
+  const [user, setUser] = React.useState({ user: null });
+
+  React.useEffect(() => {
+    setUser({ user: null });
+  }, []);
+
   return (
-    <ThemeProvider theme={theme}>
-      <Header />
-      <div className="content-wrapper">
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-        </Routes>
-      </div>
-      <Footer />
-    </ThemeProvider>
+    <AuthContext.Provider value={{ user }}>
+      <ThemeProvider theme={theme}>
+        <Header />
+        <div className="content-wrapper">
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </div>
+        <Footer />
+      </ThemeProvider>
+    </AuthContext.Provider>
+
   );
 }
 
