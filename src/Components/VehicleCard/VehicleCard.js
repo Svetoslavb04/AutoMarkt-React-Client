03cd0ad8e850a14f9b92407dc15bcf0ab4f508e6
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2022-05-05 14:36:11 +0300

    Added Card Hover Effect

--- src/Components/VehicleCard/VehicleCard.js
+++ src/Components/VehicleCard/VehicleCard.js
@@ -8,45 +8,46 @@ import './VehicleCard.scss';
 
 export default function VehicleCard() {
   return (
-    <Card sx={{ maxWidth: 275 }}>
-      <CardActionArea>
+    <Card className='vehicle-card-wrapper'>
+      <div className='vehicle-card-image-wrapper'>
         <CardMedia
+          className='vehicle-card-image'
           component="img"
           height="275"
           image={image2}
           alt="green iguana"
         />
-        <CardContent className='vehicle-card-content-wrapper'>
-          <Grid container className='vehicle-card-make-model-flex'>
-            <Grid item className='vehicle-card-make-model-cell'>
-              <Typography className='vehicle-card-make' variant="h5">
-                Ktm
-              </Typography>
-            </Grid>
-            <Grid item>
-              <Typography className='vehicle-card-model' variant="h5">
-                Exc 300
-              </Typography>
-            </Grid>
+      </div>
+      <CardContent className='vehicle-card-content-wrapper'>
+        <Grid container className='vehicle-card-make-model-flex'>
+          <Grid item className='vehicle-card-make-model-cell'>
+            <Typography className='vehicle-card-make' variant="h5">
+              Ktm
+            </Typography>
           </Grid>
-          <Divider className='vehicle-card-divider' />
-          <Grid container className='vehicle-card-make-model-flex'>
-            <Grid item className='vehicle-card-year-milage-cell'>
-              <Typography className='vehicle-card-year' variant="h6">
-                2020,
-              </Typography>
-            </Grid>
-            <Grid item>
-              <Typography className='vehicle-card-milage' variant="h6">
-                1000km
-              </Typography>
-            </Grid>
+          <Grid item>
+            <Typography className='vehicle-card-model' variant="h5">
+              Exc 300
+            </Typography>
           </Grid>
-          <Typography className='vehicle-card-price' variant="h6">
-            $5000.00
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+        </Grid>
+        <Divider className='vehicle-card-divider' />
+        <Grid container className='vehicle-card-make-model-flex'>
+          <Grid item className='vehicle-card-year-milage-cell'>
+            <Typography className='vehicle-card-year' variant="h6">
+              2020,
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography className='vehicle-card-milage' variant="h6">
+              1000km
+            </Typography>
+          </Grid>
+        </Grid>
+        <Typography className='vehicle-card-price' variant="h6">
+          $5000.00
+        </Typography>
+      </CardContent>
     </Card>
   );
 }
