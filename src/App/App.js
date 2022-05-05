d191118f022da9885dabfdcc5b4bf2fadfa71684
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2022-05-05 18:58:51 +0300

    Updated CSS Inject First

--- src/App/App.js
+++ src/App/App.js
@@ -4,7 +4,8 @@ import { Routes, Route } from 'react-router-dom';
 import { AuthProvider } from '../contexts/AuthContext.js';
 import { NotificationProvider } from '../contexts/NotificationContext.js';
 import { theme } from '../config/theme';
-import { ThemeProvider } from '@mui/material/styles';
+
+import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles';
 
 import Header from '../Components/Header/Header';
 import Footer from '../Components/Footer/Footer';
@@ -12,6 +13,7 @@ import Home from '../Pages/Home/Home';
 import Login from '../Pages/Login/Login';
 import Register from '../Pages/Register/Register';
 import Logout from '../Pages/Logout/Logout';
+import Catalog from '../Pages/Catalog/Catalog.js';
 import Notification from '../Components/Notification/Notification';
 
 import './App.scss';
@@ -20,19 +22,26 @@ function App() {
   return (
     <AuthProvider>
       <ThemeProvider theme={theme}>
-        <NotificationProvider>
-          <Header />
-          <div className="content-wrapper">
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/login' element={<Login />} />
-              <Route path='/register' element={<Register />} />
-              <Route path='/logout' element={<Logout />} />
-            </Routes>
-          </div>
-          <Footer />
-          <Notification />
-        </NotificationProvider>
+        <StyledEngineProvider injectFirst>
+          {
+            <NotificationProvider>
+              <Header />
+              <div className="content-wrapper">
+                <Routes>
+                  <Route path='/' element={<Home />} />
+                  <Route path='/login' element={<Login />} />
+                  <Route path='/register' element={<Register />} />
+                  <Route path='/logout' element={<Logout />} />
+                  <Route path="/catalog" element={<Catalog />}>
+                    <Route path=":category" element={<Catalog />} />
+                  </Route>
+                </Routes>
+              </div>
+              <Footer />
+              <Notification />
+            </NotificationProvider>
+          }
+        </StyledEngineProvider>
       </ThemeProvider>
     </AuthProvider>
   );
