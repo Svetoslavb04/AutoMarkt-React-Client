afab41ec2f120c59de644ac897cd1dc90f5300bf
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2022-05-18 19:27:16 +0300

    Added Authenticated Route

---
+++ src/Components/Routes/AuthenticatedRoute.js
@@ -0,0 +1,17 @@
+import { useAuthContext } from "../../contexts/AuthContext";
+import { Navigate } from "react-router-dom";
+
+export default function AuthenticatedRoute(props) {
+
+    const { user } = useAuthContext();
+
+    if (!user.isAuthenticated) {
+
+        return <Navigate to='/login' replace />;
+        
+    }
+
+    return (
+        props.children
+    );
+}
