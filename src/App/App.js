8f0df365773d2d32983a4208600ee4c0c5ad4374
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2022-05-18 19:34:13 +0300

    Added Wish List

--- src/App/App.js
+++ src/App/App.js
@@ -11,44 +11,53 @@ import { theme } from '../config/theme';
 
 import { ThemeProvider, StyledEngineProvider } from '../mui-imports';
 
+import AuthenticatedRoute from '../Components/Routes/AuthenticatedRoute';
+
+import Notification from '../Components/Notification/Notification';
 import Header from '../Components/Header/Header';
 import Footer from '../Components/Footer/Footer';
 import Home from '../Pages/Home/Home';
 import Login from '../Pages/Login/Login';
 import Register from '../Pages/Register/Register';
 import Logout from '../Pages/Logout/Logout';
-import Catalog from '../Pages/Catalog/Catalog.js';
-import Notification from '../Components/Notification/Notification';
-import Details from '../Pages/Details/Details.js';
+import Catalog from '../Pages/Catalog/Catalog';
+import Details from '../Pages/Details/Details';
+import WishList from '../Pages/WishList/WishList';
 
 import './App.scss';
 
 function App() {
+
   return (
     <AuthProvider>
       <ThemeProvider theme={theme}>
         <StyledEngineProvider injectFirst>
           {
             <NotificationProvider>
-            <WishListProvider>
-            <ShoppingCartProvider>
-              <Header />
-              <LoadingProvider>
-                <div className="content-wrapper">
-                  <Routes>
-                    <Route path='/' element={<Home />} />
-                    <Route path='/login' element={<Login />} />
-                    <Route path='/register' element={<Register />} />
-                    <Route path='/logout' element={<Logout />} />
-                    <Route path="/catalog" element={<Catalog />} />
-                    <Route path="/catalog/:_id" element={<Details />} />
-                  </Routes>
-                </div>
-              </LoadingProvider>
-              <Footer />
-              <Notification />
-            </ShoppingCartProvider>
-            </WishListProvider>
+              <WishListProvider>
+                <ShoppingCartProvider>
+                  <Header />
+                  <LoadingProvider>
+                    <div className="content-wrapper">
+                      <Routes>
+                        <Route path='/' element={<Home />} />
+                        <Route path='/login' element={<Login />} />
+                        <Route path='/register' element={<Register />} />
+                        <Route path='/logout' element={
+                          <AuthenticatedRoute>
+                            <Logout />
+                          </AuthenticatedRoute>
+                        } />
+                        <Route path="/catalog" element={<Catalog />} />
+                        <Route path="/catalog/:_id" element={<Details />} />
+                        <Route path="/wish-list" element={<WishList />} />
+                      </Routes>
+                    </div>
+                  </LoadingProvider>
+                  <Footer />
+                  <Notification />
+                </ShoppingCartProvider>
+              </WishListProvider>
             </NotificationProvider>
           }
         </StyledEngineProvider>
