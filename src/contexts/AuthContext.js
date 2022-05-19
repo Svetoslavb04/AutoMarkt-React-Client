23c7e3cb3768b78b429737a7c484311d96b5a95d
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2022-05-20 01:24:05 +0300

    Fix refresh bug

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -18,16 +18,16 @@ export const AuthProvider = (props) => {
         authService.authStatus()
             .then(user => {
 
-                setIsUserStateSettled(true);
-
                 if (!user) {
 
                     return setUser(initialUser);
 
                 }
 
-                setUser({ _id: user._id, username: user.username, isAuthenticated: true });
-                
+                setUser({ _id: user.user._id, username: user.user.username, isAuthenticated: true });
+
+                setIsUserStateSettled(true);
+
             });
     }, [])
 
