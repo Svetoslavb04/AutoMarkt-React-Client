f3f1693ec02dc11c296f9ef837407e46430615c6
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2022-05-21 10:13:27 +0300

    Added Notification on redirecting from Private Route

--- src/Components/Routes/AuthenticatedRoute.js
+++ src/Components/Routes/AuthenticatedRoute.js
@@ -1,12 +1,22 @@
 import { useAuthContext } from "../../contexts/AuthContext";
 import { Navigate } from "react-router-dom";
 
+import { useNotificationContext, types } from '../../contexts/NotificationContext'
+
 export default function AuthenticatedRoute(props) {
-    
+
     const { user } = useAuthContext();
-    
+
+    const { popNotification } = useNotificationContext();
+
     if (!user.isAuthenticated) {
-        
+
+        setTimeout(() => {
+
+            popNotification('Please login!', types.error);
+
+        }, 400);
+
         return <Navigate to='/login' replace />;
 
     }
